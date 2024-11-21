f05a35641e99576e8902cb5170dcacd7fc795744
IsaX01 <isaiaybadia@gmail.com>
2024-11-21 19:41:00 -0400

    config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ItemModule } from './item/item.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Item } from './item/entities/item.entity';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'TU_HOST',
+      port: 5432,
+      username: 'TU_USUARIO',
+      password: 'TU_CONTRASEÑA',
+      database: 'TU_BASE_DE_DATOS',
+      entities: [Item],
+      synchronize: true,
+    }),
+    ScheduleModule.forRoot(),
+    ItemModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.connectMicroservice({
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://TU_USUARIO:TU_CONTRASEÑA@TU_HOST:5672'],
+      queue: 'items_queue',
+      queueOptions: {
+        durable: false,
+      },
+    },
+  });
+  
+  await app.startAllMicroservices();
   await app.listen(3000);
 }
 bootstrap();
